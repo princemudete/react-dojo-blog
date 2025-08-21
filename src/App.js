37ba4ae611cb27aa652494aa19b227a11aeef52e
princemudete <princemudete@gmail.com>
2025-08-21 08:57:02 +0200

    Initial commit - job scraper project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   const title = "Welcome the Gogo";
@@ -21,6 +23,12 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
           {/* <Home /> */}
--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
         const [error, setError] = useState(null);
     
         useEffect(() => {
+            const abortCont = new AbortController();
+
             setTimeout(() => {
-                fetch(url)
+                fetch(url, { signal: abortCont.signal })
                    .then((res) => {
                     // console.log(res);
                     if(!res.ok) {
@@ -22,11 +24,17 @@ const useFetch = (url) => {
                     setError(null);
                     })
                     .catch(err => {
+                        if (err.name === "AbortError") {
+                            console.log("fetch aborted");
+                        }else {
                         setIsPending(false);
                         // console.log(err.message);
                         setError(err.message);
+                        }
                     })
-            }, 1000);   
+            }, 1000); 
+            
+            return () => abortCont.abort();
             // console.log('use effect ran');
             // console.log(name);
             // console.log(blogs);
